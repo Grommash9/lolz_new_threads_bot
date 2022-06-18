f8c4dc94b8f2b55275ca770f0f85a2088ed41293
prudn <prundikov21@icloud.com>
2022-06-18 23:15:34 +0300

    time check added

--- app/lolz_get_api.py
+++ app/lolz_get_api.py
@@ -1,3 +1,4 @@
+import json
 import time
 import requests
 from app import config, tg, data_base
@@ -15,12 +16,14 @@ def get_updates():
         json_response = response.json()
 
         for threads in json_response['threads']:
-            if data_base.add_thread(threads['thread_id']):
-                tg.send_data(f"<b>Новая тема!</b> от {threads['creator_username']}\n"
-                             f"------------------------------\n"
-                             f"{threads['thread_title']}\n"
-                             f"------------------------------\n"
-                             f"{threads['first_post']['post_body_plain_text'][:3800]}\n\n"
-                             f"{threads['links']['permalink']}")
-                time.sleep(1)
+            if threads['first_post']['post_create_date'] > time.time() - 3600:
+                if data_base.add_thread(threads['thread_id']):
+
+                    tg.send_data(f"<b>Новая тема!</b> от {threads['creator_username']}\n"
+                                 f"------------------------------\n"
+                                 f"{threads['thread_title']}\n"
+                                 f"------------------------------\n"
+                                 f"{threads['first_post']['post_body_plain_text'][:3800]}\n\n"
+                                 f"{threads['links']['permalink']}")
+                    time.sleep(1)
         time.sleep(5)
